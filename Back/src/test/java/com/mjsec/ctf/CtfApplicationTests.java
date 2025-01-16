1f39f05d70a5bda539a0eaa3e5a7c3944ded94fe
minsoo0506 <mspark010506@gmail.com>
2025-01-16 11:06:40 +0900

    chore(init): basic settings

---
+++ Back/src/test/java/com/mjsec/ctf/CtfApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mjsec.ctf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
