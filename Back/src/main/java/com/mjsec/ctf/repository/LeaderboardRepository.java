acfd1c48ededd593a6d261d425617345330ee849
이주오 <dlwndh0422@gmail.com>
2025-02-03 10:07:08 +0900

    docs: 점수 정렬 수정 근거 주석 추가  및 주석 오타 수정

--- Back/src/main/java/com/mjsec/ctf/repository/LeaderboardRepository.java
+++ Back/src/main/java/com/mjsec/ctf/repository/LeaderboardRepository.java
@@ -9,7 +9,11 @@ import java.util.List;
 @Repository
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
 
-    // 모든 데이터를 total_point 기준 내림차순으로 가져오기 (혼자 할때는 scroe 로 쓰다가 잘못넣었네요... ㅎㅎ)
+    // 모든 데이터를 total_point 기준 내림차순으로 가져오기
+    // 데이터베이스의 값이 변경되었다는 것은 정답을 맞추거나 틀리거나 둘 중 하나의 상태인 것으로 생각됩니다.
+    // 하여 정답을 맞춘 경우에도 업데이트 시간이 현재 시간으로 업데이트 되어 UpdatedAtAsc 를 통해
+    // 데이터베이스 내 updated_at 의 값을 기준으로 다시 재정렬 되게 됩니다.
+    // 테스트 시에도 후발주자가 더 낮은 순위를 갖는 것을 확인했습니다.
     List<Leaderboard> findAllByOrderByTotalPointDescUpdatedAtAsc();
 
 }
--- Back/src/main/java/com/mjsec/ctf/service/LeaderboardService.java
+++ Back/src/main/java/com/mjsec/ctf/service/LeaderboardService.java
@@ -19,7 +19,7 @@ public class LeaderboardService {
 
     // 모든 유저의 userid, total_poing, univ 데이터를 가져오는 메서드
     public List<Leaderboard> getLeaderboard() {
-        return leaderboardRepository.findAllByOrderByTotalPointDescUpdatedAtAsc(); // 모든 데이터를 score 기준 내림차순으로 가져오기
+        return leaderboardRepository.findAllByOrderByTotalPointDescUpdatedAtAsc(); // 모든 데이터를 total_point 기준 내림차순으로 가져오기
     }
 
 }
