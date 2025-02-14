41c9868da6e229dddbb79f428b2db08e3632afe6
이주오 <dlwndh0422@gmail.com>
2025-02-14 09:26:33 +0900

    feat: 점수 정렬 시 최종 문제 풀이시간 고려 정렬 방식으로 개선
    다만 대체로 DB에서의 변경이 많기 트리거를 활용하여 DB TABLE 을 사용해야 합니다.

--- Back/src/main/java/com/mjsec/ctf/entity/Leaderboard.java
+++ Back/src/main/java/com/mjsec/ctf/entity/Leaderboard.java
@@ -1,59 +1,34 @@
 package com.mjsec.ctf.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 
 @Slf4j
 @Entity
+@Getter
+@Setter
+@Table(name = "leaderboard") 
 public class Leaderboard {
-    //제가 사용한 테이블명은 Leaderboard 로 했습니다.
+
     @Id
-    private Long id; // Primary key
-    private String userid;  // varchar(255)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private String userid;
+
+    @Column(name = "total_point")
+    private int totalPoint;
+
+    @Column(name = "last_solved_time")
+    private LocalDateTime lastSolvedTime;
+
+    @Column(name = "univ")
     private String univ;
-    private int totalPoint; // int
 
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getUniv(){
-        return univ;
-    }
-    public void setUniv(String univ) {
-        this.univ = univ;
-    }
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public int getTotalPoint() {
-        return totalPoint;
-    }
-
-    public void setTotalPoint(int totalPoint) {
-        this.totalPoint = totalPoint;
-    }
 }
