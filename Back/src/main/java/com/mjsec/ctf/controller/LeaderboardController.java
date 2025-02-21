36be7015612ce789cce53eab671311b1b4a16d53
Jongcoding <ialleejy@gmail.com>
2025-02-21 14:55:52 +0900

    add import crossrorigin

--- Back/src/main/java/com/mjsec/ctf/controller/LeaderboardController.java
+++ Back/src/main/java/com/mjsec/ctf/controller/LeaderboardController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.io.IOException;
 import java.util.List;
 
