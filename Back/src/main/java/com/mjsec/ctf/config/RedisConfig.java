e7c0b7b3308f6ce7cbcaaaa7e3e03757540091e4
minsoo0506 <mspark010506@gmail.com>
2025-02-25 22:17:02 +0900

    fix(redis config): RedisConnectionFactory 추가

--- Back/src/main/java/com/mjsec/ctf/config/RedisConfig.java
+++ Back/src/main/java/com/mjsec/ctf/config/RedisConfig.java
@@ -1,13 +1,38 @@
 package com.mjsec.ctf.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    // private final RedisMessageListener redisMessageListener;
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setPassword(password);
+
+        return new LettuceConnectionFactory(configuration);
+    }
 
     @Bean
     public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
