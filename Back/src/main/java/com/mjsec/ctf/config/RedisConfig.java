69051da28f9f4973f2754784c747bdf8431ee05b
minsoo0506 <mspark010506@gmail.com>
2025-02-25 22:44:23 +0900

    fix(redis config): RedisConnectionFactory 수정

--- Back/src/main/java/com/mjsec/ctf/config/RedisConfig.java
+++ Back/src/main/java/com/mjsec/ctf/config/RedisConfig.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    // private final RedisMessageListener redisMessageListener;
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -22,12 +19,7 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-
-        configuration.setHostName(host);
-        configuration.setPort(port);
-
-        return new LettuceConnectionFactory(configuration);
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
