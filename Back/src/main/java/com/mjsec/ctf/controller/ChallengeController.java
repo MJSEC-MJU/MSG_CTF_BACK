811975fd2a6950003f4f7b0b4c9d6144c1542502
Jongcoding <ialleejy@gmail.com>
2025-02-27 01:46:54 +0900

    fix: error 코드 추가, import 추가

--- Back/src/main/java/com/mjsec/ctf/controller/ChallengeController.java
+++ Back/src/main/java/com/mjsec/ctf/controller/ChallengeController.java
@@ -11,13 +11,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.IOException;
 import org.springframework.core.io.ByteArrayResource;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/challenges")
