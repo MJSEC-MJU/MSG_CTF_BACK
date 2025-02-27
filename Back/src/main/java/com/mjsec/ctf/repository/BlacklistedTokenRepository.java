152da7abf6fda6581935ee916a3f2cf2adc79cd0
yuchan <yuchan8003@naver.com>
2025-02-27 10:24:13 +0900

    Fix: (임시용) 유저 프로필 조회시 푼 문제 정보 반환

--- Back/src/main/java/com/mjsec/ctf/repository/BlacklistedTokenRepository.java
+++ Back/src/main/java/com/mjsec/ctf/repository/BlacklistedTokenRepository.java
@@ -5,11 +5,13 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
 
+@Repository
 public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedTokenEntity,Long> {
 
     boolean existsByToken(String token);
--- Back/src/main/java/com/mjsec/ctf/repository/HistoryRepository.java
+++ Back/src/main/java/com/mjsec/ctf/repository/HistoryRepository.java
@@ -2,10 +2,13 @@ package com.mjsec.ctf.repository;
 
 import com.mjsec.ctf.domain.HistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface HistoryRepository extends JpaRepository<HistoryEntity, Long> {
     // 필요 시 사용자별 조회 등의 메소드를 추가할 수 있음
     List<HistoryEntity> findAllByOrderBySolvedTimeAsc();
+    List<HistoryEntity> findByUserId(String userId);
 }
