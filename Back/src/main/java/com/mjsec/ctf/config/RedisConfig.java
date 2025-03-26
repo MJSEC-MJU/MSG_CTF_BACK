56e0448faf982eab9d7ae3bf46ceab5125116593
minsoo0506 <mspark010506@gmail.com>
2025-03-26 18:44:29 +0900

    fix(submit): add redisson for concurrency control

--- Back/src/main/java/com/mjsec/ctf/config/RedisConfig.java
+++ Back/src/main/java/com/mjsec/ctf/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.mjsec.ctf.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,5 +38,15 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port)
+                .setConnectionPoolSize(10) // 최대 연결 수를 설정하며, 동시에 얼마나 많은 클라이언트가 Redis와 연결을 맺을 수 있을지를 결정
+                .setConnectionMinimumIdleSize(5); // 최소 유휴 연결 수를 설정하여, 연결 풀이 항상 일정 수의 연결을 유지하게 하고, 즉시 사용할 수 있는 연결을 보장
+        return Redisson.create(config);
     }
 }
